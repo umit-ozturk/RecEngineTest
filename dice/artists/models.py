f89b76e7149baabbe9bd8f4cac2a5d3da4f105c8
Umit Ozturk <umit.ozturk@deepensive.com>
2021-06-21 15:45:49 +0100

    Artist App Update List and Detail Views Done

--- dice/artists/models.py
+++ dice/artists/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
@@ -14,6 +15,15 @@ class Artist(models.Model):
     embedding = models.JSONField(_("Artist Embedding"), blank=True, null=True)
 
     image_url = models.URLField(_("Artist Image Url"), null=True, blank=True)
+
+    def get_absolute_url(self):
+        """Get url for artist's detail view.
+
+        Returns:
+            str: URL for artist detail.
+
+        """
+        return reverse("artists:detail", args=[str(self.id)])
 
     def __str__(self):
         return self.name
--- dice/artists/urls.py
+++ dice/artists/urls.py
@@ -9,7 +9,7 @@ from dice.artists.views import (
 app_name = "artists"
 
 urlpatterns = [
-    path("list/", ArtistListView.as_view(), name="list"),
-    path("~update/", ArtistUpdateView.as_view(), name="update"),
+    path("", ArtistListView.as_view(), name="list"),
+    path("<int:pk>/update/", ArtistUpdateView.as_view(), name="update"),
     path("<int:pk>/", ArtistDetailView.as_view(), name="detail"),
 ]

8e95301748ea6dacbe3ef3438b71f521f9d96e57
Umit Ozturk <umit.ozturk@deepensive.com>
2021-06-21 12:43:04 +0100

    Initial Commit

---
+++ dice/users/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+import uuid
+
+
+class User(AbstractUser):
+    """Default user for Dice."""
+    id = models.UUIDField(
+        _("ID"),
+        primary_key=True,
+        default=uuid.uuid4
+    )
+    embedding = models.JSONField(_("User Embedding"), blank=True, null=True)
+
+    #: First and last name do not cover name patterns around the globe
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    first_name = None  # type: ignore
+    last_name = None  # type: ignore
+
+    def get_absolute_url(self):
+        """Get url for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
+        return reverse("users:detail", kwargs={"username": self.username})
